4e1bb532699506c8c736257e29d6fb23c2bdea53
YIJUN <u1475897@umail.utah.edu>
2024-09-05 17:55:54 -0600

    lab2

--- Hello_Android/app/src/main/java/com/example/hello_android/MainActivity.kt
+++ Hello_Android/app/src/main/java/com/example/hello_android/MainActivity.kt
@@ -15,32 +15,40 @@ class MainActivity : AppCompatActivity() {
 
         val button1:Button = findViewById(R.id.button)
         button1.setOnClickListener{
+//            get button text
+            val buttonText = button1.text
             val intent = Intent(this,MainActivity2::class.java)
+            intent.putExtra("button_text_key", buttonText)
             startActivity(intent)
         }
 
+
         val button2:Button = findViewById(R.id.button2)
         button2.setOnClickListener{
+            val buttonText = button2.text
             val intent = Intent(this,MainActivity2::class.java)
-            startActivity(intent)
+            intent.putExtra("button_text_key", buttonText)
         }
 
         val button3:Button = findViewById(R.id.button3)
         button3.setOnClickListener{
+            val buttonText = button3.text
             val intent = Intent(this,MainActivity2::class.java)
-            startActivity(intent)
+            intent.putExtra("button_text_key", buttonText)
         }
 
         val button4:Button = findViewById(R.id.button4)
         button4.setOnClickListener{
+            val buttonText = button4.text
             val intent = Intent(this,MainActivity2::class.java)
-            startActivity(intent)
+            intent.putExtra("button_text_key", buttonText)
         }
 
         val button5:Button = findViewById(R.id.button5)
         button5.setOnClickListener{
+            val buttonText = button5.text
             val intent = Intent(this,MainActivity2::class.java)
-            startActivity(intent)
+            intent.putExtra("button_text_key", buttonText)
         }
 
     }
--- Hello_Android/app/src/main/java/com/example/hello_android/MainActivity2.kt
+++ Hello_Android/app/src/main/java/com/example/hello_android/MainActivity2.kt
@@ -2,11 +2,17 @@ package com.example.hello_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+//       get text view by id
+        val textView: TextView = findViewById(R.id.textView)
+        val buttonText = intent.getStringExtra("button_text_key") // Use the same key to retrieve the data
+        textView.text = buttonText
+
 
     }
 }
